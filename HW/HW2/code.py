46368e24dceeb46117098bca2dbe90723bff1f48
ChristopherANelson <59150814+ChristopherANelson@users.noreply.github.com>
2022-09-10 21:56:47 -0400

    Updated Code [9.10.22]

---
+++ HW/HW2/code.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 10 14:30:41 2022
+
+@author: Christopher Nelson, Alex
+
+HW-2 - EMCH 567
+"""
+
+
+import IPython as IP
+IP.get_ipython().magic('reset -sf')
+
+import numpy as np
+import scipy as sp
+import pandas as pd
+from scipy import fftpack, signal # have to add 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import sklearn as sk
+from sklearn import linear_model
+
+
+def prepare_country_stats(oecd_bli, gdp_per_capita):
+    oecd_bli = oecd_bli[oecd_bli["INEQUALITY"]=="TOT"]
+    oecd_bli = oecd_bli.pivot(index="Country", columns="Indicator", 
+                              values="Value")
+    gdp_per_capita.rename(columns={"2015": "GDP per capita"}, inplace=True)
+    gdp_per_capita.set_index("Country", inplace=True)
+    full_country_stats = pd.merge(left=oecd_bli, right=gdp_per_capita,
+                                  left_index=True, right_index=True)
+    full_country_stats.sort_values(by="GDP per capita", inplace=True)
+    remove_indices = [0, 1, 6, 8, 33, 34, 35]
+    keep_indices = list(set(range(36)) - set(remove_indices))
+    return full_country_stats[["GDP per capita", 'Life satisfaction']].iloc[keep_indices]
+   
+plt.close('all')
+
+#%% Load and plot data
+oecd_bli = pd.read_csv("data\oecd_bli_2015.csv", thousands=',')
+gdp_per_capita = pd.read_csv("data\gdp_per_capita.csv",thousands=',',
+                             delimiter='\t', encoding='latin1', na_values="n/a")
+
+# Prepare the data
+country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
+GDP = country_stats.values[:,0]
+life_satisfaction = country_stats.values[:,1]
+countries = list(country_stats.index)
+
+#%% Plot original data
+plt.figure()
+plt.xlabel('GDP')
+plt.ylabel('Life Satisfaction')
+plt.title('GDP vs Life Satisfaction')
+plt.plot(GDP,life_satisfaction,'o')
+
+
+#%% Normalization
+data = np.asarray(GDP.reshape(-1,1))
+xMax=max(data)
+xMin=min(data)
+yMax=max(life_satisfaction)
+yMin=min(life_satisfaction)
+
+X=np.asarray([(xMax-i)/(xMax-xMin) for i in data])
+Y=np.asarray([(yMax-i)/(yMax-yMin) for i in life_satisfaction])
+
+plt.figure()
+plt.plot(X,Y,'o')
